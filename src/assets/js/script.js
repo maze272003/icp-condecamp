4e2d4e4a02f5575a098f572bd64c6336f349d188
maze nats <jg.jonatas.au@phinmaed.com>
2024-12-03 04:58:58 +0000

    modify the type writter

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "website distroyer", "android hackerman", "web development"],
+    strings: ["frontend development", "backend development", "website destroyer", "android hackerman", "web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
